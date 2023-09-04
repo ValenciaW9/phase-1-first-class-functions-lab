eb4a435717bd11365b8b8186ab9cae195bb5d138
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-04 14:07:53 -0400

    phase-1-first-class-functions-lab/
    phase-1-first-class-functions-lab/

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,81 +1,49 @@
 require ( './helpers.js' );
 
-describe('index.js', function () {
-  const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const chai = require("chai");
+const spies = require("chai-spies");
+chai.use(spies);
 
-  afterEach(function () {
-    expect(drivers, 'MAKE SURE YOUR ARRAY MANIPULATIONS ARE NON-DESTRUCTIVE').to.eql(['Antonia', 'Nuru', 'Amari', 'Mo']);
-  });
+describe("index", () => {
+  describe("receivesAFunction(callback)", () => {
+    it("receives a function and calls it", () => {
+      const spy = chai.spy();
 
-  describe('returnFirstTwoDrivers()', function () {
-    it('should return a new array containing the first two drivers from the passed-in array', function () {
-      expect(returnFirstTwoDrivers(drivers)).to.eql(['Antonia', 'Nuru']);
+      receivesAFunction(spy);
+
+      expect(spy).to.have.been.called();
     });
   });
 
-  describe('returnLastTwoDrivers()', function () {
-    it('should return an array of the last two drivers', function () {
-      expect(returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])).to.eql(['Amari', 'Mo']);
+  describe("returnsANamedFunction()", () => {
+    var fn;
+
+    before(() => {
+      fn = returnsANamedFunction();
+    });
+
+    it("returns a function", () => {
+      expect(fn).to.be.a("function");
+    });
+
+    it("returns a named function", () => {
+      expect(fn.name).not.to.eql("");
     });
   });
 
-  describe('selectingDrivers', function () {
-    it('has the `returnFirstTwoDrivers` function to as its first element', function () {
-      expect(selectingDrivers[0]).to.eql(returnFirstTwoDrivers);
+  describe("returnsAnAnonymousFunction()", () => {
+    var fn;
+
+    before(() => {
+      fn = returnsAnAnonymousFunction();
     });
 
-    it('has the `returnLastTwoDrivers` function to as its last element', function () {
-      expect(selectingDrivers[1]).to.eql(returnLastTwoDrivers);
+    it("returns a function", () => {
+      expect(fn).to.be.a("function");
     });
 
-    it('allows us to invoke either function from the array', function () {
-      expect(selectingDrivers[0](drivers)).to.eql(['Antonia', 'Nuru']);
-
-      expect(selectingDrivers[1](drivers)).to.eql(['Amari', 'Mo']);
-    });
-  });
-
-  describe('createFareMultiplier()', function () {
-    it('returns a function', function () {
-      const fareMultiplier = createFareMultiplier(2);
-
-      expect(fareMultiplier).to.be.a('function');
-    });
-
-    it('should multiply a given value using the created multiplier', function () {
-      const fareQuintupler = createFareMultiplier(5);
-
-      expect(fareQuintupler(5)).to.eql(25);
-    });
-  });
-
-  describe('fareDoubler()', function () {
-    it('is a function', function () {
-      expect(fareDoubler).to.be.a('function');
-    });
-
-    it('doubles fares', function () {
-      expect(fareDoubler(10)).to.eql(20);
-    });
-  });
-
-  describe('fareTripler()', function () {
-    it('is a function', function () {
-      expect(fareTripler).to.be.a('function');
-    });
-
-    it('triples fares', function() {
-      expect(fareTripler(12)).to.eql(36);
-    });
-  });
-
-  describe('selectDifferentDrivers(arrayOfDrivers, function)', function () {
-    it('returns the first two drivers when passed returnFirstTwoDrivers() as the second argument', function () {
-      expect(selectDifferentDrivers(drivers, returnFirstTwoDrivers)).to.eql(['Antonia', 'Nuru']);
-    });
-
-    it('returns the last two drivers when passed returnLastTwoDrivers() as the second argument', function () {
-      expect(selectDifferentDrivers(drivers, returnLastTwoDrivers)).to.eql(['Amari', 'Mo']);
+    it("returns an anonymous function", () => {
+      expect(fn.name).to.eql("");
     });
   });
 });
